15ba0d66182d921fb40fde4d9c31d6076ec6ec5e
apple juice <strbac.jelena.js@gmail.com>
2021-01-19 12:41:32 +0100

    [fix]: fixed notation when capturing is done with non-pawn pieces

--- src/utils/writeNotation.js
+++ src/utils/writeNotation.js
@@ -2,8 +2,10 @@ const rows = [8, 7, 6, 5, 4, 3, 2, 1];
 const cols = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 const writeNotation = (figure, r, c, captured, prevCol) => {
-  if (captured) {
+  if (captured && figure[1] === "P") {
     return `${cols[prevCol]}x${cols[c]}${rows[r]}`;
+  } else if (captured && figure[1] !== "P") {
+    return `${figure[1]}x${cols[c]}${rows[r]}`;
   }
   return `${figure[1] !== "P" ? figure[1] : ""}${cols[c]}${rows[r]}`;
 };
