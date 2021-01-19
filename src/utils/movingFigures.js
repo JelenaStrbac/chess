04622ebbf2f2e00797abb1608edf200385f6dee5
apple juice <strbac.jelena.js@gmail.com>
2021-01-19 18:03:19 +0100

    [feat]: rook movment

--- src/utils/movingFigures.js
+++ src/utils/movingFigures.js
@@ -1,4 +1,5 @@
 const movements = {
+  // *** PAWN *** //
   P: (board, player, currentRow, currentCol, notation) => {
     let squaresArr = [];
     const currRow = Number(currentRow);
@@ -83,6 +84,51 @@ const movements = {
       )
         squaresArr.push(`${currRow + 2}-${currCol}`);
     }
+    return squaresArr;
+  },
+  // *** ROOK *** //
+  R: (board, player, currentRow, currentCol) => {
+    const squaresArr = [];
+    const currRow = Number(currentRow);
+    const currCol = Number(currentCol);
+
+    // up
+    for (let i = currRow - 1; i >= 0; i--) {
+      if (board[i][currCol] !== null && board[i][currCol]?.[0] !== player) {
+        squaresArr.push(`${i}-${currCol}`);
+        break;
+      }
+      if (board[i][currCol] !== null) break;
+      squaresArr.push(`${i}-${currCol}`);
+    }
+    // down
+    for (let i = currRow + 1; i <= 7; i++) {
+      if (board[i][currCol] !== null && board[i][currCol]?.[0] !== player) {
+        squaresArr.push(`${i}-${currCol}`);
+        break;
+      }
+      if (board[i][currCol] !== null) break;
+      squaresArr.push(`${i}-${currCol}`);
+    }
+    // left
+    for (let i = currCol - 1; i >= 0; i--) {
+      if (board[currRow][i] !== null && board[currRow][i]?.[0] !== player) {
+        squaresArr.push(`${currRow}-${i}`);
+        break;
+      }
+      if (board[currRow][i] !== null) break;
+      squaresArr.push(`${currRow}-${i}`);
+    }
+    // right
+    for (let i = currCol + 1; i <= 7; i++) {
+      if (board[currRow][i] !== null && board[currRow][i]?.[0] !== player) {
+        squaresArr.push(`${currRow}-${i}`);
+        break;
+      }
+      if (board[currRow][i] !== null) break;
+      squaresArr.push(`${currRow}-${i}`);
+    }
+
     return squaresArr;
   },
 };
