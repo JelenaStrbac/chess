9935bffdfa38e054048352d0f84a5784955d24d5
apple juice <strbac.jelena.js@gmail.com>
2021-02-04 11:02:23 +0100

    [feat-prev]: added reseted state for rematch

---
+++ src/utils/gameFlowHelpers/resetStateForRematch.js
@@ -0,0 +1,35 @@
+export const resetStateForRematch = (state) => {
+  state.board = [
+    ["BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR"],
+    ["BP", "BP", "BP", "BP", "BP", "BP", "BP", "BP"],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    ["WP", "WP", "WP", "WP", "WP", "WP", "WP", "WP"],
+    ["WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"],
+  ];
+  state.activePlayer = "W";
+  state.activePlayerStatus = "selecting";
+  state.current = {
+    field: "",
+    figure: "",
+  };
+  state.possibleMoves = [];
+  state.notation = [];
+  state.captured = {
+    W: [],
+    B: [],
+  };
+  state.pawnPromotion = {
+    W: "Q",
+    B: "Q",
+  };
+  state.end = {
+    isGameEnded: false,
+    howIsGameEnded: "", // checkmate, draw, resign
+    winner: "",
+    loser: "",
+    isRematch: true,
+  };
+};
